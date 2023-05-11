774b30ce07178c8897d812137e15d4a3336297fc
athulpm <97athulpm@gmail.com>
2023-05-11 09:12:14 +0530

    getters and mapGetter

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,12 +1,18 @@
 <template>
-  <p>Counter is: {{ counter }}</p>
+  <div>
+    <p>Counter is: {{ doubleCounter }}</p>
+    <p>Number of clicks: {{ stringCounter }}</p>
+    <p>Triple is: {{ triple }}</p>
+  </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 export default {
   computed: {
-    counter() {
-      return this.$store.state.counter;
+    ...mapGetters(["doubleCounter", "stringCounter"]),
+    triple() {
+      return this.$store.state.counter * 3;
     },
   },
 };
